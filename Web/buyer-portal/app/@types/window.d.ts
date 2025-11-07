ebfba038b0194bbf2d699c5eb74cc98e9064df69
Natalie Wiggins <natalie.emily.wiggins@gmail.com>
2025-11-07 11:09:56 +0000

    Typecheck buyer-portal

--- Web/buyer-portal/app/@types/window.d.ts
+++ Web/buyer-portal/app/@types/window.d.ts
@@ -1,9 +1,9 @@
 interface Window {
   ethereum?: {
     isMetaMask?: true;
-    request?: (...args: any[]) => Promise<any>;
-    on?: (...args: any[]) => void;
-    removeListener?: (...args: any[]) => void;
-    removeAllListeners?: (...args: any[]) => void;
+    request: (...args: any[]) => Promise<any>;
+    on: (...args: any[]) => void;
+    removeListener: (...args: any[]) => void;
+    removeAllListeners: (...args: any[]) => void;
   };
 }
--- Web/buyer-portal/next.config.js
+++ Web/buyer-portal/next.config.js
@@ -3,13 +3,7 @@ const nextConfig = {
   swcMinify: false,
   images: {
     domains: ['images.pexels.com'],
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  }
 }
 
 module.exports = nextConfig
